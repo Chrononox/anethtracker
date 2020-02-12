ebbcdfbaf18b83ca9c8b824b2835a3205d9ffb9e
Travis <tegbert@gmail.com>
2020-02-12 13:28:49 -0600

    codin

--- src/App.js
+++ src/App.js
@@ -8,33 +8,27 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      currtime: '',
-      hr: '',
-      resp: '',
-      temp: '',
-      spo2: '',
-      co2: '',
-      bp: '',
-      iso: '',
-      ivf: '',
-      ekg: '',
-      test: 'O.O',
+      currtime: '00:00',
+      hr: '00',
+      resp: '00',
+      temp: '00.0',
+      spo2: '00',
+      co2: '00',
+      bp: '00',
+      iso: '00',
+      ivf: '00',
+      ekg: '000',
     }
   }
 
   //set the time when button is clicked  use =new Date() https://tecadmin.net/get-current-date-time-javascript/
   setcurrtime(event){
-    this.setState({time: 'time value'})
-    console.log('in time')
+    console.log('Time not working yet')
   }
 
-  ///Data State Setters.  
- hr(event){
-    console.log('in HR')
-  }
-
-  // setHr =   (event) => this.setState({hr: event.target.value})
+  ///Data State Setters.
   setresp = (event) => this.setState({resp: event.target.value});
+  sethr =   (event) => this.setState({hr: event.target.value})
   settemp = (event) => this.setState({temp: event.target.value});
   setspo2 = (event) => this.setState({spo2: event.target.value});
   setco2 =  (event) => this.setState({co2: event.target.value});
@@ -58,9 +52,16 @@ class App extends React.Component{
         </div>
         <div className='form'>
           <Form
-            time ={this.state.time} setcurrtime={this.setcurrtime}
+            time={this.state.time} setcurrtime={this.setcurrtime}
+            hr={this.state.hr} sethr={this.sethr}
             resp={this.state.resp} setresp={this.setresp}
-            
+            temp={this.state.temp} settemp={this.settemp}
+            spo2={this.state.spo2} setspo2={this.setspo2}
+            co2={this.state.co2} setco2={this.setco2}
+            bp={this.state.bp} setbp={this.setbp}
+            iso={this.state.iso} setiso={this.setiso}
+            ivf={this.state.ivf} setivf={this.setivf}
+            ekg={this.state.ekg} setekg={this.etekg}
           />
         </div>
       </div>
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,28 +4,20 @@ import DataInput from './DataInput'
 
 //this form will gather all of the vital signs of the PT send off to be placed in a array to be printed out
 
-// const Form = ({ resp, setresp }) =>{
-// return(
-//   <div className=''>
-//     {/* <p>Time: <input onChange={setTime} /></p>
-//     <p>HR: <input onChange={setHr} /></p> */}
-//     <DataInput title={'Resp'} data={resp} setFunc={setresp}/>
-
-//   </div>
-// )
-// }
-
-class Form extends React.Component{
- 
-
-  render(){
-    return(
-      <div>
-        <p>{this.props.test}</p>
-      </div>
-    )
-  }
+const Form = ({ time, hr, resp, temp, spo2, co2, bp, iso, ivf, ekg, setcurrtime,sethr,  setresp, settemp, setspo2, setco2, setbp, setiso, setivf, setekg }) =>{
+  return(
+    <div className=''>
+      <DataInput title={'Time'} data={time} setFunc={setcurrtime}/>
+      <DataInput title={'HR'} data={hr} setFunc={sethr}/>
+      <DataInput title={'Resp'} data={resp} setFunc={setresp}/>
+      <DataInput title={'Temp'} data={temp} setFunc={settemp}/>
+      <DataInput title={'SPO2'} data={spo2} setFunc={setspo2}/>
+      <DataInput title={'CO2'} data={co2} setFunc={setco2}/>
+      <DataInput title={'BP'} data={bp} setFunc={setbp}/>
+      <DataInput title={'ISO'} data={iso} setFunc={setiso}/>
+      <DataInput title={'IVF'} data={ivf} setFunc={setivf}/>
+      <DataInput title={'EKG'} data={ekg} setFunc={setekg}/>
+    </div>
+  )
 }
-
-
 export default Form;
