004d7b9d511e43fee932342aba92103065b0d0b0
Chrononox <tegbert@gmail.com>
2020-02-13 09:29:46 -0600

    added form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import './App.css';
-
+import Form from './Components/Form/Form'
 
 
 
 
 class App extends React.Component{
+  
   constructor(){
     super();
     this.state = {
+      time: '00:00',
+      hr: '000',
+      resp: '00',
+      temp: '00.0',
+      spo2: '00', //display as %
+      co2: '00',
+      bp: '000/00 (000)', //may break up bp to three parts
+      iso: '0.0',
+      ivf: '00',  //display in ml/hr
+      ekg: 'NNN',
     }
   }
+
+  //setState functions for vitals(form)
+  sethr  = (event) => this.setState({hr: event.target.value})
+  setresp =(event) => this.setState({resp: event.target.value})
+  settemp =(event) => this.setState({temp: event.target.value})
+  setspo2 =(event) => this.setState({spo2: event.target.value})
+  setco2 =(event) => this.setState({co2: event.target.value})
+  setbp =(event) => this.setState({bp: event.target.value})
+  setiso =(event) => this.setState({iso: event.target.value})
+  setivf =(event) => this.setState({ivf: event.target.value})
+  setekg =(event) => this.setState({ekg: event.target.value})
+
   render(){
     return(
       <div>
+        <Form 
+          hr={this.state.hr} sethr={this.sethr}
+          resp={this.state.resp} setresp={this.setresp}
+          temp={this.state.temp} settemp={this.settemp}
+          spo2={this.state.spo2} setspo2={this.setspo2}
+          co2={this.state.co2} setco2={this.setco2}
+          bp= {this.state.bp} setbp={this.setbp}
+          iso={this.state.iso} setiso={this.setiso}
+          ivf={this.state.ivf} setivf={this.setivf}
+          ekg={this.state.ekg} setekg={this.setekg}
+        />
       </div>
     )
   }
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import DataInput from './DataInput'
-import ExportButton from './ExportButton'
+
 
 
 //this form will gather all of the vital signs of the PT send off to be placed in a array to be printed out
 
-const Form = ({ time, hr, resp, temp, spo2, co2, bp, iso, ivf, ekg, setcurrtime,sethr,  setresp, settemp, setspo2, setco2, setbp, setiso, setivf, setekg, settoarray }) =>{
+const Form = ({ time, hr, resp, temp, spo2, co2, bp, iso, ivf, ekg, setcurrtime, sethr, setresp, settemp, setspo2, setco2, setbp, setiso, setivf, setekg, settoarray }) =>{
   return(
     <div className=''>
-      <div>
-        <DataInput title={'Time'} data={time} setFunc={setcurrtime}/>
-        <DataInput title={'HR'} data={hr} setFunc={sethr}/>
-        <DataInput title={'Resp'} data={resp} setFunc={setresp}/>
-        <DataInput title={'Temp'} data={temp} setFunc={settemp}/>
-        <DataInput title={'SPO2'} data={spo2} setFunc={setspo2}/>
-        <DataInput title={'CO2'} data={co2} setFunc={setco2}/>
-        <DataInput title={'BP'} data={bp} setFunc={setbp}/>
-        <DataInput title={'ISO'} data={iso} setFunc={setiso}/>
-        <DataInput title={'IVF'} data={ivf} setFunc={setivf}/>
-        <DataInput title={'EKG'} data={ekg} setFunc={setekg}/>
-      </div>
-      <button onClick={settoarray}>save</button>
+      <p>HR: <input onChange={sethr} placeholder={hr}/></p>
+      <p>RESP: <input onChange={setresp} placeholder={resp}/></p>
+      <p>TEMP: <input onChange={settemp} placeholder={temp}/></p>
+      <p>SPO2: <input onChange={setspo2} placeholder={spo2+'%'}/></p>
+      <p>CO2: <input onChange={setco2} placeholder={co2}/></p>
+      <p>BP: <input onChange={setbp} placeholder={bp}/></p>
+      <p>ISO: <input onChange={setiso} placeholder={iso}/></p>
+      <p>IVF: <input onChange={setivf} placeholder={ivf+' ml/hr'}/></p>
+      <p>EKG: <input onChange={setekg} placeholder={ekg}/></p>
+      
     </div>
   )  
 }
